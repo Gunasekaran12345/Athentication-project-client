c072ac4898d42839f385973ad6858dcbed15da4d
unknown <gunaguna6142@gmail.com>
2023-10-18 23:41:07 -0700

    second message

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,12 +1,19 @@
 import React from "react";
+import "./Home.css";
 import { Link } from "react-router-dom";
 const Home = () => {
   return (
-
-        <Link to="/">
-          <div className="button2">Submit</div>
-        </Link>
-  );
+    <div className="Container">
+<nav>
+  <ul>
+    <li><Link to="/">login</Link></li>
+    <li><Link to="/Register">Register</Link></li>
+    <li><a href="#">logout</a></li>
+  </ul>
+</nav>
+</div>
+    
+  )
 };
 
 export default Home;
